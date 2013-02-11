0e0951af14e660167f82f642e6bceea8fdd73673
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-02-11 17:14:28 +0100

    Added 'node' actor

---
+++ actors/node.js
@@ -0,0 +1,26 @@
+define(
+
+    function () {
+
+        return function (cfg) {
+
+            if (!cfg.node) {
+                throw "param expected: node";
+            }
+
+            var scene = this.getObject("scene");
+
+            var nodeId = cfg.nodeId || "world";
+
+            var node = scene.getNode(nodeId);
+            if (!node) {
+                throw "scene node not found: " + nodeId ;
+            }
+
+            var root = node.addNode(cfg.node);
+
+            this._destroy = function () {
+                root.destroy();
+            };
+        };
+    });
